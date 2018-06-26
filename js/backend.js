bb755c0f403ce14fecc64a921d0fb68fb8a43559
AngryFennec <shellfix@gmail.com>
2018-06-26 16:51:17 +0300

    Переделала backend.js

--- js/backend.js
+++ js/backend.js
@@ -1,43 +1,40 @@
 'use strict';
 //xhr.open('GET', '');
 (function () {
-  window.load = function (url, onSuccess, onError) {
-    var xhr = new XMLHttpRequest();
 
-    xhr.responseType = 'json';
-
-    xhr.addEventListener('load', function () {
-      if (xhr.status === 200) {
-        onSuccess(xhr.response);
-      } else {
-        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
-      }
-    });
-
-    xhr.addEventListener('error', function () {
-      onError('Произошла ошибка соединения');
-    });
-
-    xhr.addEventListener('timeout', function () {
-      onError('Запрос не успел выполниться за ' + xhr.timeout + 'мс');
-    });
-
-    xhr.timeout = 10000; // 10s
-
-    xhr.open('GET', url);
-    xhr.send();
-  }
-/*
-  (function () {
-  var onError = function (message) {
+  function onError(message) {
     console.error(message);
   };
 
   var onSuccess = function (data) {
     console.log(data);
   };
-
-  window.load('https://js.dump.academy/keksobooking/data', onSuccess, onError);
-  */
-
+  window.load = function (url, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    var urlGet = 'https://js.dump.academy/keksobooking/data';
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.responseType = 'json';
+    xhr.open('GET', urlGet);
+    xhr.send();
+  };
+  window.send = function (data, onLoad, onError) {
+    var xhr = new XMLHttpRequest();
+    var urlPost = 'https://js.dump.academy/keksobooking';
+    xhr.addEventListener('load', function () {
+      if (xhr.status === 200) {
+        onLoad(xhr.response);
+      } else {
+        onError('Cтатус ответа: ' + xhr.status + ' ' + xhr.statusText);
+      }
+    });
+    xhr.responseType = 'json';
+    xhr.open('POST', urlPost);
+    xhr.send();
+  };
 })();
